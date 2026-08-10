03b5be2b1814c99f7e1c757341da5558d0d57569
alexsanyum <sanyumale@gmail.com>
2026-08-10 16:25:55 -0300

    Script to install and create blast via conda

---
+++ src/install_blast_conda.sh
@@ -0,0 +1,28 @@
+#"#/bin/bash"
+
+
+# Install BLAST+ using conda 
+if ! command -v conda &> /dev/null
+then
+    echo "conda could not be found. Please install conda first."
+    exit 1
+fi
+
+# Control the conda environment to avoid conflicts with other packages
+if conda info --envs | grep -q "blast_env"; then
+    echo "The conda environment 'blast_env' already exists."
+    echo "Use next command to remove it"
+    echo "conda env remove -n blast_env"
+    exit 1
+fi
+
+# Create a new conda environment for BLAST+, and isntall BLAST+ in it
+conda create -n blast_env -c bioconda blast -y
+
+# Activate the environment to use BLAST+
+eval "$(conda shell.bash hook)"
+conda activate blast_env
+
+# Print the BLAST+ version to verify installation
+echo "BLAST+ installation completed. The installed version is:"
+blastn -version
